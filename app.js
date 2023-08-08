01b3841f8604eda2cd6c3e6d485dfc5ea4f9e066
SzymonHarajda <31370918+SzymonHarajda@users.noreply.github.com>
2023-08-08 13:22:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let boxSize=16;
+const grid_main=document.querySelector("#grid")
+let gridValue = document.querySelector('#grid-size');
+let gridSize = document.querySelector('input');
+const gridReset = document.querySelector('#reset');
+
+let boxSize=8;
 
 /*
 for tworzy grid planszy 
@@ -8,17 +13,34 @@ suwak wyznacza wielkosc podzialki
 guzik za randomowy kolor i czarny 
 guzik czyszczcy plansze
 guziki dzialaja na onclick event 
-
 */
-grid();
+
+function createBox(size){
+        var div = document.createElement("div");
+        div.classList.add('box');
+        div.style.width = `${size}px`;
+        div.style.height = `${size}px`;
+  return div;
+}
+grid(boxSize);
 //main fucntion to trace grid 
-function grid(){
+function grid(gridSize){
     for(let i =0;i<boxSize;i++){
         for(let j =0;j<boxSize;j++){
-        var grid = document.createElement("div");
-        grid.className = "box";
-        document.getElementById('grid').appendChild(grid);
-        
-        }  
+  grid_main.appendChild(createBox(grid_main.clientWidth/gridSize));
+         }  
     }
 }
+grid_main.addEventListener('mouseover', function (e) {
+  if (e.target.matches('.box')) {
+    e.target.classList.add('active');
+  }
+});
+function reset(){
+  while(grid_main.firstChild){
+    grid_main.removeChild(grid_main.lastChild)
+  }
+  grid(boxSize);
+}
+
+gridReset.addEventListener('click', reset)
